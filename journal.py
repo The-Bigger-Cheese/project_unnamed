825c6475cdd281f939cb7675a53b8481a8e4d2a2
AZAL-34 <azal34000@gmail.com>
2025-02-14 10:16:32 +0000

    gfsdgfgdfgadfagdfg

--- journal.py
+++ journal.py
@@ -4,11 +4,13 @@ import csv
 with open("active_save.txt", "r") as active:
     save_key = active.read()
 
-printy("Enter the ID of the entry you wish to view.")
-with open(f"journal_{save_key}.txt", "r") as journal:
-    for id in journal.readlines():
-        print(id)
+journal = open(f"journal_{save_key}.txt", "r")
 
+printy("Enter the ID of the entry you wish to view.")
+for id in journal.readlines():
+    print(id, end = "")
+
+# pretty sure it's this bit that doesn't work properly...
 select = "oi stop looking at the code"
 while select not in journal.readlines():
     printy("Select an ID from above.")
